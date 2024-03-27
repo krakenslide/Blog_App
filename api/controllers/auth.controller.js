38ca92cae1553ea5cb064a8e06541fad03b31cf5
Ankit Mukhopadhyay <muk.ankit@gmail.com>
2024-03-27 19:46:15 +0530

    SignIn done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 
 const signup = asyncHandler(async (req, res) => {
   const { email } = req.body;
@@ -16,4 +17,27 @@ const signup = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { signup };
+const signin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const findUser = await User.findOne({ email: email });
+    if (!findUser || !(await findUser.isPasswordMatched(password))) {
+      throw new Error("Invalid Credentials");
+    }
+    const accessToken = jwt.sign({ id: findUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1",
+    });
+    const { password: omitPassword, ...userData } = findUser.toObject();
+    res.cookie("accessToken", accessToken, { httpOnly: true });
+    res.json({
+      success: true,
+      findUser: userData,
+      token: accessToken,
+      message: "SignIn Successful",
+    });
+  } catch (error) {
+    throw new Error("Invalid Credentials");
+  }
+});
+
+module.exports = { signup, signin };

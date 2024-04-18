d1118ba3413a7a9454fc6aac7c3d824931689394
Ankit Mukhopadhyay <muk.ankit@gmail.com>
2024-04-18 22:52:19 +0530

    Api base check

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
         target: "https://blog-app-f85t.onrender.com",
         secure: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },

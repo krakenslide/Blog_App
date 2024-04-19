b478c260aa303ce2a9046e9a42b5159c543fa099
Ankit Mukhopadhyay <muk.ankit@gmail.com>
2024-04-20 03:16:33 +0530

    Api endpoint changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://blog-app-8j8t.onrender.com",
-        secure: true,
-      },
+      "/api": "https://blog-app-8j8t.onrender.com",
     },
   },
   plugins: [react()],
